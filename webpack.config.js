f27273e546950f824dd1963acbf813b99f2964c1
hariprasad <hariprasadkethini@gmail.com>
2024-09-29 16:29:06 +0530

    wepack config changed from development to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "assets/images",
+              name: '[name].[ext]',
+              outputPath: 'assets/images',
             },
           },
         ],
       },
     ],
   },
-  mode: "development",
+  mode: 'production',
 };
